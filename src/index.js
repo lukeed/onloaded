b96ffca543eca7c1d786ed5c6baf00081c1ee267
Luke Edwards <luke.edwards05@gmail.com>
2017-07-30 23:45:07 -0700

    initial module logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-import mitt from 'mitt';
+import forEach from '@arr/foreach';
 
-const emitter = mitt();
+function noop() {}
 
-export default function (str) {
+export default function (elem, opts) {
+	opts = opts || {};
+
+	if (typeof elem === 'string') {
+		elem = document.querySelectorAll(elem);
+	} else if (elem.length == void 0) {
+		elem = [elem]; // was single elem
+	}
+
+	let ok = err = 0;
+	const total = elem.length;
+
+	function isDone() {
+		let perc = (ok + err) / total;
+		let func = perc === 1 ? opts.onComplete : opts.onProgress;
+		(func || noop)(perc, { failed:err, loaded:ok, total });
+	}
+
+	function onload() {
+		ok++ && (opts.onLoad || noop)(this) && isDone();
+	}
+
+	function onerror() {
+		err++ && (opts.onError || noop)(this) && isDone();
+	}
+
+	forEach(elem, el => {
+		el.onload = onload;
+		el.onerror = onerror;
+	});
 }
