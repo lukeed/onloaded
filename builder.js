73b9fa65b0ec0a713af234696c2e59f43d8a2fa6
Luke Edwards <luke.edwards05@gmail.com>
2017-07-30 18:28:46 -0700

    install `mitt`; import builder script

---
+++ builder.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const { resolve } = require('path');
+const { minify } = require('uglify-js');
+const pretty = require('pretty-bytes');
+const sizer = require('gzip-size');
+const pkg = require('./package');
+
+const umd = pkg['umd:main'];
+
+rollup({
+	entry: 'src/index.js',
+	useStrict: false
+}).then(bun => {
+	bun.write({
+		format: 'cjs',
+		dest: pkg.main,
+		exports: 'default'
+	});
+
+	bun.write({
+		format: 'es',
+		dest: pkg.module,
+		exports: 'default'
+	});
+
+	bun.write({
+		dest:umd,
+		format: 'umd',
+		exports: 'default',
+		moduleName: pkg.name
+	}).then(_ => {
+		const data = fs.readFileSync(umd, 'utf8');
+
+		// produce minified output
+		const { code } = minify(data, { fromString:true });
+		fs.writeFileSync(umd, code);
+
+		// output gzip size
+		const int = sizer.sync(code);
+		console.log(`> gzip size: ${ pretty(int) }`);
+	});
+});
